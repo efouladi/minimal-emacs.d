b4d98c452beedf7d8dc58e7977cc4f876778fffd
shafo <shafo@posto.net>
2026-01-10 22:01:38 +0100

    Add which key

--- post-init.el
+++ post-init.el
@@ -326,3 +326,14 @@
   :custom
   ;; Improve readability of diffs
   (magit-diff-refine-hunk 'all))
+
+(use-package which-key
+  :ensure nil ; builtin
+  :commands which-key-mode
+  :hook (after-init . which-key-mode)
+  :custom
+  (which-key-idle-delay 1.5)
+  (which-key-idle-secondary-delay 0.25)
+  (which-key-add-column-padding 1)
+  (which-key-max-description-length 40))
+
